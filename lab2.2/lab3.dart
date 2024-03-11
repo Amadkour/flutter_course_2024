9199bae97e78b652ab86299c944fd78428eed4b6
Ahmed <amira.gomaa@res.inc>
2024-03-12 01:57:11 +0200

    lab-3

--- lab2.2/lab3.dart
+++ lab2.2/lab3.dart
@@ -1,5 +1,3 @@
-
-
 /// constractor VS named Constractor
 /// super, this keyword
 /// abstract vs interface, concrete
@@ -9,6 +7,69 @@
 /// collection [list, map]
 /// stack and heap for primitive and refrence variables
 /// is and as keywords
+
+void main(List<String> args) {
+  B b = B("Ahmed", 25, 10);
+  b.fun1(10);
+}
+
+class C {
+  C(this.address) {
+    print("normal constractor" + address);
+  }
+  String address = 'tanta';
+  void fun1(int a) {
+    print(a);
+  }
+}
+//================[inhertance for concrete or abstract class]=================//
+
+class B extends C {
+  late String name = "";
+  late int age;
+  // B(String name, int age) : super(10) {
+  //   this.name = name;
+  //   this.age = age;
+  // }
+  B(this.name, this.age, super.address) {}
+  fun2() {
+    fun1(10);
+  }
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return super.toString();
+  }
+
+  @override
+  void fun1(int a) {
+    print("override from class B");
+  }
+}
+//================[interface]=================//
+
+class A implements C {
+  @override
+  int a = 10;
+
+  @override
+  void fun1(int a) {
+    print("from override");
+  }
+}
+
+//===============[mixin]===========//
+mixin X {
+  fun1() {}
+}
+
+class Z with X {
+  fun2() {
+    fun1();
+  }
+}
+
 
 
 
